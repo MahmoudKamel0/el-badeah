dd87ceacead3945a722ec60f366e9a7a7ae208aa
Mahmoud Kamel <kamelmahmoud335@gmail.com>
2025-01-10 02:41:04 +0300

    build v1

--- src/app/components/OfferAdsLatest/OfferAdsLatest.tsx
+++ src/app/components/OfferAdsLatest/OfferAdsLatest.tsx
@@ -12,13 +12,13 @@ export default function OfferAdsLatest() {
        return (
               <section className="offersAdsLatest | flex flex-auto p-5 gap-5 mt-10" dir="rtl">
               <div className="startSide | flex-auto grid grid-cols-1 overflow-hidden rounded-lg">
-                     <Image className="object-cover h-full w-full" src="/offers ads/1 main.webp" alt="startSide" loading="lazy" width="500" height="500"  />
+                     <Image className="object-cover h-full w-full" src="/offersAds/1 main.webp" alt="startSide" loading="lazy" width="500" height="500"  />
               </div>
               <div className="endSide | flex-auto grid grid-cols-2 gap-5">
-                     <Image className="object-cover h-full w-full" src="/offers ads/2.webp" alt="startSide" loading="lazy" width="300" height="300"  />
-                     <Image className="object-cover h-full w-full" src="/offers ads/3.webp" alt="startSide" loading="lazy" width="300" height="300"  />
-                     <Image className="object-cover h-full w-full" src="/offers ads/4.webp" alt="startSide" loading="lazy" width="300" height="300"  />
-                     <Image className="object-cover h-full w-full" src="/offers ads/5.webp" alt="startSide" loading="lazy" width="300" height="300"  />
+                     <Image className="object-cover h-full w-full" src="/offersAds/2.webp" alt="startSide" loading="lazy" width="300" height="300"  />
+                     <Image className="object-cover h-full w-full" src="/offersAds/3.webp" alt="startSide" loading="lazy" width="300" height="300"  />
+                     <Image className="object-cover h-full w-full" src="/offersAds/4.webp" alt="startSide" loading="lazy" width="300" height="300"  />
+                     <Image className="object-cover h-full w-full" src="/offersAds/5.webp" alt="startSide" loading="lazy" width="300" height="300"  />
               </div>
               </section>
        )
